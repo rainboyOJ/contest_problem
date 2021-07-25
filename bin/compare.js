59b75870a0841767541056e50a8ec4fda931d5d9
rainboy <rainboylvx@qq.com>
2021-07-25 10:21:13 +0800

    update

---
+++ bin/compare.js
@@ -0,0 +1,59 @@
+/**
+ * 手动比较 结果
+ */
+
+var fs = require('fs');
+var path = require('path');//解析需要遍历的文件夹
+const child_process = require("child_process")
+
+var getDataList= require("./getDataList")
+
+function Diff(file1,file2){
+  //TODO check file1 2 exists?
+  try {
+    child_process.execSync(`diff ${file1} ${file2}`)
+    return 1
+  }
+  catch(error){
+    return 0;
+  }
+}
+
+function run(time,exe,in_file,out){
+  //TODO check file1 2 exists?
+  try {
+    child_process.execSync(`timeout ${time} ${exe} < ${in_file} > ${out}`)
+    return 1
+  }
+  catch(error){
+    return 0;
+  }
+}
+
+/* 
+ * 1. 执行文件
+ * 2. 数据目录
+ * */
+function compare( exePath,dataDir ,time=1){
+  var { in_list,out_list,both_list,count } = getDataList(dataDir)
+
+  for(let i =0 ; i < count ;i++){
+      let [in_file,out_file] = both_list[i]
+      let __in = path.join(dataDir,in_file)
+      let file1 = path.join(dataDir,out_file)
+      let file2 = 'out'
+
+      if( run(exePath, time , __in, file2) == 0){
+          console.log( `${i}: timeout` )
+          continue
+      }
+
+      if( Diff(file1,file2) == 0){
+          console.log( `${i}: wrong answer` )
+          continue
+      }
+      console.log( `${i}: right` )
+  }
+}
+
+console.log( process.argv.slice(2) )
